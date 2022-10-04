160776d204a81928f37d9712dbf21e7acc6a1d90
unknown <sasyayan@yandex.by>
2022-10-04 22:24:36 +0800

    added comment and review models. Model progress(1/2)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Avg
 
 User = get_user_model()
 
@@ -13,7 +14,37 @@ class Review(models.Model):
     average_score - ср.значение рейтинга."""
 
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='author')
-    score = models.DecimalField(max_digits=10, decimal_places=0)
+        User, on_delete=models.CASCADE, related_name='reviewer'
+        )
+    score = models.DecimalField(
+        max_digits=10, decimal_places=0
+        )
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
+        )
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='review'
+        )
+
+    # def average_score(self):
+    #     average_score = Review.objects.filter(title=title).aggregate(Avg('score'))
+    #     return average_score
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    """Класс комментария. Связывает произведение, автора и текст."""
+
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name='commentator'
+    )
+    review = models.ForeignKey(
+        Review, on_delete=models.SET_NULL, related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
