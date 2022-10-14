4162c00021ab1365f58463272ff83a26ff10454b
Саша Айдарова <sasyayan@yandex.by>
2022-10-14 11:05:48 +0800

    ?

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,102 @@
+import datetime
+
+from django.db.models import Avg
+from rest_framework import serializers
+from reviews.models import Comment, Review
+from titles.models import Genre, Categorie, Title
+from users.models import User
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор Genre (Жанры)."""
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class CategorieSerializer(serializers.ModelSerializer):
+    """Сериализатор Categorie (категории)."""
+
+    class Meta:
+        model = Categorie
+        fields = '__all__'
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Сериализатор Title (Произведения)."""
+    genre = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='name'
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Categorie.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+    def validate_year(self, value):
+        """Валидация года."""
+        if value['not_valid']:
+            raise serializers.ValidationError("Not valid")
+        if self.year > datetime.date.today().year:
+            raise serializers.ValidationError("Not valid")
+        return value
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+    )
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=['author', 'title'],
+                message='Пользователь уже писал рецензию на произведение!')
+        ]
+
+    def validate_score(self):
+        """Проверка оценки произведения на вхождение в диапазон от 1 до 10. """
+        score = self.context['request'].score
+        if score < 1 and score > 10:
+            raise serializers.ValidationError(
+                'Оценка произведений от 1 до 10.')
+
+    def get_rating(self, obj):
+        """Вычисление среднего рейтинга произведения."""
+        rating = Review.objects.filter(
+            title=obj.title).annotate(Avg('score'))
+        return rating
+
+
+class SignUpSerializator(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=30, required=True)
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class GetTokenSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=30, required=True)
+    confirmation_code = serializers.CharField(max_length=60, required=True)
---
+++ api_yamdb/reviews/management/commands/csv_to_SQL.py
@@ -0,0 +1,36 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    help = 'Creating model objects according the file path specified'
+    data = os.listdir(path='static/data')
+    title_app_models = ['Genre', 'Titles', 'Categorie', 'Title_genre']
+    review_app_models = ['Review', 'Comment']
+    user_app_models = ['User', ]
+
+    def handle(self, *args, **options):
+        for file in self.data:
+            with open(f'static/data/{file}', encoding='utf-8') as csv_file:
+                name_file = file.split('.')[0]
+                app_label = ''
+                if name_file.title() in self.user_app_models:
+                    app_label = 'users'
+                elif name_file.title() in self.review_app_models:
+                    app_label = 'reviews'
+                else:
+                    app_label = 'titles'
+                    print(name_file)
+                model = apps.get_model(
+                    app_label=app_label, model_name=name_file.title())
+                reader = csv.reader(csv_file, delimiter=',', quotechar='"',
+                                    skipinitialspace=True)
+                header = next(reader)
+                for row in reader:
+                    print(row)
+                    _object_dict = {key: value for key,
+                                    value in zip(header, row)}
+                    model.objects.create(**_object_dict)
